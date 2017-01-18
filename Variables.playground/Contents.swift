5336a5f0292f07e7f5ab1b88a1da60757a3f26ea
Jeroen de Bie <Jeroendebie@live.nl>
2017-01-18 10:05:28 +0100

    Playground of yesterday 17-01-17

---
+++ Variables.playground/Contents.swift
@@ -0,0 +1,179 @@
+import Cocoa
+
+// = Dit teken gebruik je wanneer je commentaar geeft op een stuk code.
+
+//int=getal var=variables let=constants string=letters.
+
+//String interpolation = lets you combine constant and variable valau
+
+//let = een constante
+
+//var = variable waarde.
+
+//integer = nummer.  voorb: var integer: int = 0
+
+//character = een woord. voorb: Var: Character = "D"
+
+//String = een woord of een aaneensluiting van woorden. voorb: var stringVar: String = "this is a string"
+
+//Float = een accuraat getal met meerdere getalken.  voorb: Var: Float = 1.2232
+
+//Double = een kort getal met een .   voorb: var d: Double = 1.023
+
+//Boot of Boolesan = true of false voorb: var isItRaining: Bool = false
+
+//Constant = kun je niet meer wijzigen het is een constante, gebruik als iets wil beveiligen of iets consant is
+
+
+let numberOfStoplights:Int = 4
+var population: Int = 5422
+let townName = "Leusden"
+let townDescription = "\(townName) has a population of \(population) people and \(numberOfStoplights) stopligts."
+print(townDescription)
+
+let numberOfStoplights2:Int = 27
+var population2: Int = 130000
+let townName2 = "Amersfoort"
+let townDescription2 = "\(townName2) has a population of \(population2) people and \(numberOfStoplights2) stopligts."
+print(townDescription2)
+
+var NN = "Amersfoort has a population of 130000 people and 27 stopligts."
+print(NN)
+
+print(population2)
+
+var result = 5 * 4
+var result1 = 2 + 3
+print(result + result1)
+
+var calc = (5 * 3)/(2 * 2)
+print(calc)
+
+let a = 3
+let b = 5
+let average = (a * b) / 2
+print(average)
+
+var c: Double = 3.5
+var d = 20
+var result2 = c + Double(d)
+
+let Fahrenheit: Double = 3.5
+var Celsius = (Fahrenheit * 1.8) + 32
+
+var count = 0
+count = count + 0
+count += 2
+
+var decrement = 0
+decrement -= 2
+
+print("Hello World")
+
+var sun = "The sun"
+print(sun)
+
+var iphone = 3
+iphone = 7
+
+var  book = "Enter the Dragon"
+print("Enter the Dragon \(sun)")
+
+print("\(Celsius) \(sun)")
+
+var town50: Int = 30000
+var town60 = 3500
+
+var age = 16
+print(age)
+
+age = 30
+print(age)
+
+//Boolean
+
+var rain:Bool = false
+var sun5:Bool = false
+
+if rain {
+    print("raining")
+} else {
+    print("clear")
+}
+if sun5 {
+    print("sunny")
+} else {
+    print("tunder")
+}
+
+//Concatenate variables: combi of text an a variable
+var age8 = 35
+var name = "Ben"
+print("I am called \(name) I am \(age8) years old")
+
+//Hieronder probeer ik een constante (Let) te veranderen, wat niet man ;-)
+let constant = "I am Ben"
+//constant = "who are you?"
+
+var Result9 = 1 + 1
+print(Result9)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
