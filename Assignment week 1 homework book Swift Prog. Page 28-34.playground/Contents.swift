79c346ee161b9746162612bc18b7f5a0b368f314
Jeroen de Bie <Jeroendebie@live.nl>
2017-01-22 22:38:18 +0100

    22-01-17 (Homework week 1 Book Swift prog. page 28-34)

---
+++ Assignment week 1 homework book Swift Prog. Page 28-34.playground/Contents.swift
@@ -0,0 +1,72 @@
+//: Homework week 1 Book Swift prog. page 28-34
+import UIKit
+
+//OPERATIONS ON INTEGERS//
+let numberOfPeople: UInt = 40
+let volumeAdjustment: Int32 = 1000
+
+//print(10 + 20)
+//print(30 - 5)
+//print(5 * 6)
+
+//print(10 + 2 * 5) // 20, because 2 * 5 is evaluated first.
+//print(30 - 5 - 5) // 20, because 30 - 5 is evaluated first.
+
+//print((10 + 2) * 5) // 60, because ((10 + 2) is now evaluated first.
+//print(30 - (5 - 5)) // 30, because (5 - 5) is now evaluated first.
+
+
+//INTEGER DEVISION//
+
+//print(11 / 3) // Prints 3
+
+//print(11 % 3) // Prints 2
+//print(-11 % 3) // Prints -2
+
+
+//OPERATOR SHORTHAND//
+
+//var x = 10
+//x += 10 // Is eqivalent to: x = x + 10
+//print("x has had 10 added to it and is now \(x)")
+//x -= 5 // Is equivalent to: x = x - 5
+//print("x has had 5 subtracted from it and is now \(x)")
+
+
+//OVERFLOW OPERATORS//
+
+//let y: Int8 = 120
+//let z = y &+ 10
+//print("129 &+ 10 is \(z)")
+
+
+//CONVERTING BETWEEN INTEGER TYPES//
+
+//let a:Int16 = 200
+//let b:Int8 = 50
+//let c = a + Int16(b)
+
+
+//FLOATING-POINT NUMBERS//
+
+let d1 = 1.1 // Implicitly Double
+let d2: Double = 1.1
+let f1: Float = 100.3
+//
+//print(10.0 + 11.4)
+//print(11.0 / 3.0)
+//
+//if d1 == d2 {
+//    print("d1 and d2 are the same!")
+//    }
+
+//print("d1 + 0.1 is \(d1 + 0.1)")
+//if d1 + 0.1 == 1.2 {
+//    print("d1 + 0.1 is equal to 1.2")
+//}
+
+
+
+
+
+
