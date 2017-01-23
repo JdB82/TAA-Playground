4813aab4aef33c7c9850f9c5801a7b7922062ee8
Jeroen de Bie <Jeroendebie@live.nl>
2017-01-23 11:16:36 +0100

    Homework week 1 -(Part 2)Control Flow, switch, if, loop, arrays, comparison operators

---
+++ Homework week 1 -(Part 2)Control Flow, switch, if, loops, arrays, comparison operators.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+//CONTROL FLOW//
+//Logical Operators ( && and     || or     ! not )
+//Comparison Operators (< less than,  <= less than or equal, > greater than, >= greater than or equal,  == equal to,  != not equal to)
+
+//1:
+
+var isMyIphoneStillWorking: Bool = false
+
+if isMyIphoneStillWorking {
+    print("I can still call my girlfriend")
+} else {
+    print("Bommer I must send a letter")
+}
+
+
+//2:
+
+var benzine = 3
+if benzine <= 3 {
+    print("Must go to the gasstation")
+} else {
+    print("I can still drive around whitout worry's")
+}
+
+var
+
