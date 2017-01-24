9e72a2080fdba6beb227d4b9093044e598b36576
Jeroen de Bie <Jeroendebie@live.nl>
2017-01-24 17:37:58 +0100

    24-01-17 Chapter 15.

---
+++ MosterTown/MosterTown/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MosterTown
+//
+//  Created by Jeroen de Bie on 24/01/2017.
+//  Copyright © 2017 Jeroen de Bie. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
