c14e5253e53b4840f9eb5a5cc8264c36ffad38b8
Jeroen de Bie <Jeroendebie@live.nl>
2017-01-26 21:24:21 +0100

    Presentation material 26-01-17

---
+++ 26-01-17 Presentation material.playground/Contents.swift
@@ -0,0 +1,60 @@
+import UIKit
+
+// Basic Variables and Constant types //
+
+//var or let int         = 5
+//var or let character   = J
+//var or let String      = "Hello, playground"
+//var or let Float       = 1.5332
+//var or let double      = 2.33211445
+//var or let Bool        = true or false
+
+//bool //string interpolation
+
+// IF or ELSE //
+
+//var bicycleSpeed: Int = 23
+//var message: String
+//
+//if bicycleSpeed < 25 {
+//    message = "\(bicycleSpeed)km/h Come on Speed up!!"
+//} else {
+//    message = "\(bicycleSpeed)km/h WOW your got the speed of a Pro!"
+//}
+
+
+
+// Bool //
+
+var slicesOfBread: Int = 50
+var message: String
+
+if slicesOfBread < 5 {
+    message = "\(slicesOfBread) slices of bread is not enough go to the supermarket and buy some bread"
+} else {
+    message = "\(slicesOfBread) slices of bread is enough for everybody."
+}
+
+print(message)
+
+
+var reining: Bool = true
+if reining {
+    print("I get wet")
+}
+
+
+//While loops.
+
+
+
+
+//SWitch statements.
+
+
+
+
+
+//Data structures (array).
+
+//Classes and inheritance
