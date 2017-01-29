ff82ba2a3b6dbc3210f803c128275ff98e17d746
Jeroen de Bie <Jeroendebie@live.nl>
2017-01-29 13:12:04 +0100

    Swifty Chapter one practice 26-01-17

---
+++ Swifty Chapter one pratice 26-01-17.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let str1 = "Sir"
+let str2 = "Lancelot"
+let hero = str1 + " " + str2
+print(hero)
+
+var myNumber = 42 / 2
+var myString: String
+let part1 = "Let's be"
+let part2 = " Friends!"
+var output = part1 + part2
+print(output)
+
+
+
+
+
+
+
+
+
+
