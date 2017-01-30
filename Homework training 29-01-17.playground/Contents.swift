a8ffe958fb00b61e3c1355401e3c6d43bd93c90d
Jeroen de Bie <Jeroendebie@live.nl>
2017-01-30 17:34:06 +0100

    Homework training 29-01-17

---
+++ Homework training 29-01-17.playground/Contents.swift
@@ -0,0 +1,73 @@
+//: Special for Ben //
+import UIKit
+
+//If/else
+
+//let result = 6 < 10
+//
+//if(result) { print("to late")
+//}
+
+//var result = 6 < 10 ? "six is less than ten" : "six is not less than ten"
+//print(result)
+
+
+
+//// Switch
+
+//let lateOrNotToBeLate = false
+//switch lateOrNotToBeLate {
+//case true:
+//    print("You must come out of bed sooner")
+//case false:
+//    print("Early bird")
+//}
+//
+//
+//
+//let car = "Mustang"
+//
+//if car == "Mustang" {
+//    print("The Best")
+//} else {
+//    print("The Worse")
+//}
+//var result = (car == "Mustang") ? "The Best" : " The Worse"
+//print(result)
+//
+//var age: Int = 51
+//
+//var home = ["door", "window", "roof", "woodpanel", "stonewall"]
+//for homeItem in home {
+//    print(homeItem)
+//}
+//
+//home.remove(at: 3)
+//print(home)
+//
+//home.append("gardendoor")
+//print(home)
+
+
+
+//
+//Bool
+//
+//var raining: Bool = false
+//
+//if raining {
+//    print("I get wet")
+//} else {
+//    print("Hello sun!")
+//}
+//
+
+
+
+
+
+
+
+
+
+		
