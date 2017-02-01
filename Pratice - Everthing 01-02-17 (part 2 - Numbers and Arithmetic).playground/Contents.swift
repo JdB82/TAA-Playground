b0b6cc638cb87e3efcb03113d3f44be597f72548
Jeroen de Bie <Jeroendebie@live.nl>
2017-02-01 15:09:19 +0100

    Practice - Everything 01-02-17 (part 2 - Numbers and Arithmetic , Mixing Strings and numbers)

---
+++ Pratice - Everthing 01-02-17 (part 2 - Numbers and Arithmetic).playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+// 1)
+
+var number1: Int = 10
+
+
+// 2)
+
+var number2: Int = 15
+
+
+// 3 and 4)
+
+var result: Double = Double(number1 + number2)
+
+
+// 5)
+
+var result1 = (number1 * number2)
+
+
+// 6) 
+
+number1 = 100
+
+
+// 7)
+
+var result2 = (number1 - 10 * number2)
+
+
+// 8)
+
+print(result2)
+
+
+            // Mixing Strings and number //
+
+var age: Int = 35
+var name: String = "Jeroen"
+
+print("\(name) \(age)")
+
+
+
+
+
+
+
