2fe237942d7ece129e65f3f649344eca1a405e34
Jeroen de Bie <Jeroendebie@live.nl>
2017-02-01 15:44:34 +0100

    Pratice - Everthing 01-02-17 (part 3 - Expressions and While loops )

---
+++ Pratice - Everthing 01-02-17 (part 3 - Expressions and While loops ).playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// 1 - 4)
+
+var number1 = 121
+var number2 = 15.8
+
+
+// 2 - 3)
+
+var expressionResult = (number1 < 100) && (number2 >= 10)
+
+// 5 - 6 - 7)
+
+while (number1 >= 0) {
+       number1 -= 10
+    print(number1)
+}
+
+
+
+
+
+
+
+
