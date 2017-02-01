bf61c921cd14e25c8ec9874bcff1ba338b4b2085
Jeroen de Bie <Jeroendebie@live.nl>
2017-02-01 14:45:40 +0100

    Enum practice

---
+++ Enum pratice, using playground..playground/Contents.swift
@@ -0,0 +1,54 @@
+
+import UIKit
+
+//define an enum.
+
+// 1)
+
+//enum CompassPoint {
+//    case north
+//    case south
+//    case east
+//    case west
+//    case SouthEast
+//}
+
+//print(CompassPoint.north)
+
+// 2)
+
+//var direction  = CompassPoint.north
+//var direction1 = CompassPoint.south
+//var direction2 = CompassPoint.east
+//var direction3 = CompassPoint.west
+
+
+// 3)
+
+//switch direction {
+//    
+//    case .north:
+//        print("Go north")
+//
+//    case .south:
+//        print("Go south")
+//    
+//    case .east:
+//        print("Go east")
+//    
+//    case .west:
+//        print("Go west")
+//    case. .SouthEast
+//          print("Go SouthEast")
+//}
+
+
+
+// 4) 
+
+//case SouthEast
+
+
+// 5)
+.
+
