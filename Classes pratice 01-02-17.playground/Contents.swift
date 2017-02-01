e1b3c862e73431bf14235e70a2fdfd2d8251789e
Jeroen de Bie <Jeroendebie@live.nl>
2017-02-01 14:44:45 +0100

    Classes practice 01-02-17

---
+++ Classes pratice 01-02-17.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class school {
+    var teacher: Int = 12
+    var students: Int = 430
+    var classRoom: Int = 15
+    var schoolBuilding: Int = 1
+    var studyCost: Int = 
+}
+
+    func StudyCostForAYear() {
+        <#function body#>
+    }
+    
+    
+    
+    
+    
+    
+//    init(teacher: Int, students: Int, classRoom: Int, schoolBuilding: Int, studyCost: Double) {
+//        self.teacher = teacher
+//        self.students = students
+//        self.
+//        
+//    }
+
+}
