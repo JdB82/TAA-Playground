3a720eddd83b641ddca66b1b3b28b4e9201d4ce7
Jeroen de Bie <Jeroendebie@live.nl>
2017-02-23 22:20:07 +0100

    no message

--- 24-01-17 class work -Functions IN OUT work Sheet.playground/Contents.swift
+++ 24-01-17 class work -Functions IN OUT work Sheet.playground/Contents.swift
@@ -4,20 +4,20 @@ import UIKit
 
 //parameter types, perhaps try the exercises in the book out before attempting this one… they will explain it more, but basically using the inout keyword before the parameter type -- func programming( tiredOfProgramming tired: inout_Bool) -> String { -- allows the function to change the value of the variable:
 
-                    // In-out parameters exercises out the book page 118 //
-
-var error = "The request failed:"
-func appendErrorCode(_ code: Int, toErrorString errorString: inout String) {
-    if code == 400 {
-        errorString += " bad request."
-    }
-}
-appendErrorCode(400, toErrorString: &error)
-
-
-
-1//
-
+//                    // In-out parameters exercises out the book page 118 //
+//
+//var error = "The request failed:"
+//func appendErrorCode(_ code: Int, toErrorString errorString: inout String) {
+//    if code == 400 {
+//        errorString += " bad request."
+//    }
+//}
+//appendErrorCode(400, toErrorString: &error)
+//
+//
+//
+//1//
+//
 //func programming( tiredOfProgramming tired: inout Bool, numberOfHoursProgrammed hours: Int) -> String {
 //    tired = false
 //    guard tired else {
@@ -28,4 +28,7 @@ appendErrorCode(400, toErrorString: &error)
 
 //var amITired: Bool = true
 //programming(tiredOfProgramming: &amITired, numberOfHoursProgrammed: 10000)
+
+var number = 5
+number;: Int = 2
 
